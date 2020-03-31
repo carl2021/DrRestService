3eb4381e5edf99b3561c205404bc20e49855335a
Carl Guarin Dreslet <carl0572@edu.easj.dk>
2020-03-31 10:42:28 +0200

    *

---
+++ DrRestService/Controllers/RecordsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecordsLib.Model;
+
+namespace DrRestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class RecordsController : ControllerBase
+    {
+        private static List<Records> recordsList = new List<Records>()
+        {
+            new Records(1, "Wack", "Carl", 11),
+            new Records(2, "REEEEEE", "Jonas", 120)
+        };
+
+
+        // GET: api/Records
+        [HttpGet]
+        public IEnumerable<Records> Get()
+        {
+            return recordsList;
+        }
+
+        // GET: api/Records/5
+        [HttpGet("{id}", Name = "Get")]
+        public Records Get(int id)
+        {
+            return recordsList.Find(i => i.Id == id);
+        }
+
+        // POST: api/Records
+        [HttpPost]
+        public void Post([FromBody] Records value)
+        {
+            recordsList.Add(value);
+        }
+
+        // PUT: api/Records/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Records value)
+        {
+            Records record = Get(id);
+            if (record != null)
+            {
+                record.Id = value.Id;
+                record.Title = value.Title;
+                record.Artist = value.Artist;
+                record.SongDuration = value.SongDuration;
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Records record = Get(id);
+            recordsList.Remove(record);
+        }
+    }
+}
---
+++ Records/Model/Records.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecordsLib.Model
+{
+    public class Records
+    {
+        private int _id;
+        private string _title;
+        private string _artist;
+        private int _songDuration;
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set => _artist = value;
+        }
+
+        public int SongDuration
+        {
+            get => _songDuration;
+            set => _songDuration = value;
+        }
+
+        public Records(int id, string title, string artist, int songDuration)
+        {
+            _id = id;
+            _title = title;
+            _artist = artist;
+            _songDuration = songDuration;
+        }
+   
+
+        
+    }
+}
